7570228ea9cd31a914c262c1f2af8d12b5e9d08e
Riskii <34557522+Riskii-Hcf@users.noreply.github.com>
2018-01-22 13:31:15 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,12 @@
 const Discord = require('discord.js');
  
 const bot = new Discord.Client();
+
+const yt = require('ytdl-core');
+
+var config = require("./config.json");
+
+var music = require("./playlist.json");
  
 const token = "Hell Nah";
  
@@ -243,6 +249,77 @@ if(message.channel.type === 'dm') {
   guilds[message.guild.id].prefix = newPrefix;
   message.reply(`New prefix for **${message.guild.name}** is **${newPrefix}** ! :x: **NOTICE:** Prefix cannot be changed to music part, sorry. | PLEASE USE 1 LETTER PREFIX, YOU CAN GLITCH SOME COMMANDS!`);
 }
+break;
+		
+case "ping":
+message.channel.send("Pinging...").then(pong => {
+        pong.edit(`Pong!\n${bot.user.tag}: **${pong.createdTimestamp - message.createdTimestamp}ms**\nAPI **${Math.round(bot.ping)}ms**`)
+      })
+break;
+case "music":
+message.delete("music");
+const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel){
+      return message.channel.sendMessage(":x: You are not in a voice channel!!");
+    }
+    voiceChannel.join()
+message.channel.sendMessage("```Music Module Started```")
+message.channel.sendMessage(prefix + "next " + music[Math.floor(Math.random() * music.length)]);
+break;
+case "skip":
+message.delete("skip");
+const voice = message.member.voiceChannel;
+if(perms.includes(message.author.id)) {
+voice.leave();
+voice.join()
+
+} else {
+  message.channel.sendMessage(":x: Sorry No Perms")
+}
+break;
+
+
+
+
+
+}
+});
+
+bot.on("message", function(message) {
+
+if (!message.content.startsWith(prefix)) return;
+
+var args = message.content.substring(prefix.length).split(" ");
+
+
+
+
+switch (args[0].toLowerCase()) {
+case "next":
+message.delete("next");
+const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel){
+      return message.channel.sendMessage(":x: You are not in a voice channel!!");
+    }
+    voiceChannel.join()
+    .then(connection => {
+
+      let stream = yt(args.join(" "), {audioonly: true});
+      yt.getInfo(args.join(" "), function(err, info) {
+      const title = info.title
+	  message.channel.sendMessage(`Now playing \`${title}\``)
+      })
+      const dispatcher = connection.playStream(stream);
+      dispatcher.on('end', () => {
+        var myVar = setInterval(myTimer ,10000);
+function myTimer() {
+  message.channel.sendMessage(prefix + "next " + config.music[Math.floor(Math.random() * config.music.length)]);
+clearInterval(myVar)
+}
+       }).catch(e =>{
+         console.error(e);
+       });
+    })
 break;
  
  
